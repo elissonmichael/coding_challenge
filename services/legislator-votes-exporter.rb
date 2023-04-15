3105a42110fa675e83790516044447a8afefed2d
Élisson Michael <elisson@les.inf.puc-rio.br>
2023-04-15 10:30:00 -0300

    Add Legislator Voter Exporter

---
+++ services/legislator-votes-exporter.rb
@@ -0,0 +1,24 @@
+require_relative '../model/legislator'
+
+class LegislatorVoterExporter
+  attr_reader :filename, :headers
+
+  def initialize(filename: nil)
+    @filename = filename || 'legislators-support-oppose-count.csv'
+    @headers = %w[id name num_supported_bills num_opposed_bills]
+  end
+
+  def execute
+    CSV.open(filename, 'wb', write_headers: true, headers: headers) do |csv|
+      Legislator.all.each { |legislator| csv << convert_to_row(legislator) }
+    end
+  end
+
+  private
+
+  def convert_to_row(legislator)
+    [legislator.id, legislator.name, legislator.supported_bills.size, legislator.opposed_bills.size]
+  end
+end
+
+LegislatorVoterExporter.new.execute
