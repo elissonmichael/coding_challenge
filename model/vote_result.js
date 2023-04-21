cfb0fcbbc8046df312893358645968f96ffa8e9f
Élisson Michael <elisson@les.inf.puc-rio.br>
2023-04-21 13:15:24 -0300

    Add Vote Result Model Class

--- model/vote_result.js
+++ model/vote_result.js
@@ -1,6 +1,29 @@
-import ApplicationModel from './application_model.js';
+import ApplicationModel from "./application_model.js";
 
 export default class VoteResult extends ApplicationModel {
-  static filename = 'vote_results';
+  static filename = "vote_results";
 
-};
+  static all() {
+    return this.read().then((array) =>
+      array.map(
+        (row) => new this(row.id, row.legislator_id, row.vote_id, row.vote_type)
+      )
+    );
+  }
+
+  constructor(id, legislator_id, vote_id, vote_type) {
+    super();
+    this.id = id;
+    this.legislator_id = legislator_id;
+    this.vote_id = vote_id;
+    this.vote_type = vote_type;
+  }
+
+  support() {
+    return this.vote_type === "1";
+  }
+
+  opposed() {
+    return this.vote_type === "2";
+  }
+}
